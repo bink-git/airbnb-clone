e2d9899b69fd551f87062b5745a0c80271a0be50
BINK <bink@email.ua>
2023-10-23 16:56:54 +0300

    add navbar

---
+++ app/layout.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Navbar from "./components/navbar/Navbar";
+import ClientOnly from "./components/ClientOnly";
+import Modal from "./components/modals/Modal";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Airbnb",
+  description: "Airbnb clone",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <ClientOnly>
+          <Navbar />
+          <Modal />
+        </ClientOnly>
+        {children}
+      </body>
+    </html>
+  );
+}
